d4bfac096bf9c7b6083f618c92652b13c3852240
Udit Senapaty <uditsenapaty@gmail.com>
2024-11-04 20:42:30 +0530

    Delete models directory

--- models/HeroContent.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const HeroContentSchema = new mongoose.Schema({
-  headline: String,
-  subtext: String,
-  ctaLabel: String
-});
-
-module.exports = mongoose.model('HeroContent', HeroContentSchema);
